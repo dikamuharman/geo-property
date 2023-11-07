58782439074bb13c6aaf3f6a02cdfc3def4c4bc6
dikamuharman <dika1254@gmail.com>
2023-11-07 20:49:49 +0700

    init project

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import { Container } from '@chakra-ui/react';
+import { FC } from 'react';
+
+interface NavbarProps {}
+
+const Navbar: FC<NavbarProps> = () => {
+  return <Container>Navbar</Container>;
+};
+
+export default Navbar;
---
+++ src/layout/HomeLayout.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+interface HomeLayoutProps {}
+
+const HomeLayout: FC<HomeLayoutProps> = () => {
+  return;
+};
+
+export default HomeLayout;
