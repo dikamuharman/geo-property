94ccde36b5491c154bcd4326f3a0c97a206b8069
dikamuharman <dika1254@gmail.com>
2023-11-22 10:52:52 +0700

    feat : create custom select component

---
+++ src/components/CustomSelect/CustomSelect.tsx
@@ -0,0 +1,75 @@
+import { Icon, Text } from '@chakra-ui/react';
+import { Select, chakraComponents } from 'chakra-react-select';
+import { FC } from 'react';
+import { FaMapMarkerAlt, FaSearch } from 'react-icons/fa';
+
+interface CustomSelectProps {
+  options: {
+    label: string;
+    value: string;
+  }[];
+  onChange: (value: string | undefined) => void;
+  placeholder?: string;
+  isClearable?: boolean;
+  value?: string;
+  name?: string;
+}
+
+const CustomSelect: FC<CustomSelectProps> = ({
+  options,
+  onChange,
+  placeholder,
+
+  name,
+}) => {
+  return (
+    <Select
+      size="lg"
+      placeholder={placeholder}
+      onChange={(valueOption) => onChange(valueOption?.value)}
+      options={options}
+      name={name}
+      isClearable
+      chakraStyles={{
+        input: (provided) => ({
+          ...provided,
+          width: '100%',
+        }),
+        container: (provided) => ({
+          ...provided,
+          width: '100%',
+        }),
+        dropdownIndicator: (provided) => ({
+          ...provided,
+          bg: 'transparent',
+          borderWidth: 0,
+        }),
+        indicatorSeparator: (provided) => ({
+          ...provided,
+          bg: 'transparent',
+          borderWidth: 0,
+        }),
+        crossIcon: (provided) => ({
+          ...provided,
+          color: 'blue.500',
+        }),
+      }}
+      components={{
+        Option: ({ children, ...props }) => (
+          <chakraComponents.Option {...props}>
+            <Icon as={FaMapMarkerAlt} boxSize={4} color="gray.500" mr="10px" />
+            <Text>{children}</Text>
+          </chakraComponents.Option>
+        ),
+        DropdownIndicator: ({ children, ...props }) => (
+          <chakraComponents.DropdownIndicator {...props}>
+            <Icon as={FaSearch} boxSize={4} color="blue.500" />
+            {children}
+          </chakraComponents.DropdownIndicator>
+        ),
+      }}
+    />
+  );
+};
+
+export default CustomSelect;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,15 +4,11 @@ import {
   Flex,
   HStack,
   Heading,
-  Icon,
-  Input,
-  InputGroup,
-  InputRightElement,
   Stack,
   Text,
 } from '@chakra-ui/react';
 import { FC } from 'react';
-import { MdSearch } from 'react-icons/md';
+import CustomSelect from '../components/CustomSelect/CustomSelect';
 
 interface HomePageProps {}
 
@@ -38,17 +34,23 @@ const HomePage: FC<HomePageProps> = () => {
         </Box>
         <Box bg="white" borderRadius="xl" p="7">
           <HStack>
-            <InputGroup>
-              <Input size="lg" placeholder="Masukkan lokasi atau alamat" />
-              <InputRightElement top="1">
-                <Icon as={MdSearch} boxSize={8} color="blue.500" />
-              </InputRightElement>
-            </InputGroup>
+            <CustomSelect
+              options={[
+                { label: 'Pancoran mas, Depok', value: 'Pancoran mas, Depok' },
+                { label: 'Beji, Depok', value: 'Beji, Depok' },
+              ]}
+              placeholder="Masukan lokasi (Khusus daerah depok)"
+              onChange={(value) => {
+                console.log(value);
+              }}
+              isClearable
+            />
+
             <Button colorScheme="blue" size="lg">
               Cari properti
             </Button>
           </HStack>
-          <Text color="blue.500">*khusus daerah kota depok</Text>
+          {/* <Text color="blue.500">*khusus daerah kota depok</Text> */}
         </Box>
       </Stack>
     </Flex>
