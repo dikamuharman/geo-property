db8c6220e1209a5df2672269d0ab435dccc38e2f
dikamuharman <dika1254@gmail.com>
2023-11-23 16:48:57 +0700

    chore : change load map from useEffect to useCallback

--- src/features/map/component/Map.tsx
+++ src/features/map/component/Map.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@chakra-ui/react';
 import mapboxgl from 'mapbox-gl';
-import { FC, useEffect, useRef, useState } from 'react';
+import { FC, useCallback, useRef, useState } from 'react';
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_KEY as string;
 console.log(import.meta.env.MAPBOX_KEY);
@@ -8,24 +8,39 @@ console.log(import.meta.env.MAPBOX_KEY);
 interface MapProps {}
 
 const Map: FC<MapProps> = () => {
-  const mapContainer = useRef<HTMLDivElement | null>(null);
+  // const mapContainer = useRef<HTMLDivElement | null>(null);
   const map = useRef<mapboxgl.Map | null>(null);
   const [lng] = useState(-70.9);
   const [lat] = useState(42.35);
   const [zoom] = useState(9);
 
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    console.log('render map once');
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current!,
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [lng, lat],
-      zoom: zoom,
-    });
-  });
+  // useEffect(() => {
+  //   if (map.current) return; // initialize map only once
+  //   console.log('render map once');
+  //   map.current = new mapboxgl.Map({
+  //     container: mapContainer.current!,
+  //     style: 'mapbox://styles/mapbox/streets-v12',
+  //     center: [lng, lat],
+  //     zoom: zoom,
+  //   });
+  // });
 
-  return <Box ref={mapContainer} h="full" />;
+  const mapWrapper = useCallback(
+    (wrapper: HTMLDivElement) => {
+      if (wrapper === null) return;
+      if (map.current) return;
+      console.log('render map once');
+      map.current = new mapboxgl.Map({
+        container: wrapper,
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [lng, lat],
+        zoom: zoom,
+      });
+    },
+    [lat, lng, zoom]
+  );
+
+  return <Box ref={mapWrapper} h="full" />;
 };
 
 export default Map;
