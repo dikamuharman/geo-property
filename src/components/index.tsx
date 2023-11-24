4b6b9d057ec7b25d6ee50818623d96be04813207
dikamuharman <dika1254@gmail.com>
2023-11-24 21:03:44 +0700

    feat : create component input number

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,15 @@
 import CustomSelect from './CustomSelect/CustomSelect';
 import CustomBadge from './customBadge/CustomBadge';
+import CustomNumberInput from './customFormControl/CustomNumberInput';
 import CustomTextArea from './customFormControl/CustomTextArea';
 import CustomTextField from './customFormControl/CustomTextField';
 import Navbar from './navbar/Navbar';
 
-export { CustomBadge, CustomSelect, CustomTextArea, CustomTextField, Navbar };
+export {
+  CustomBadge,
+  CustomNumberInput,
+  CustomSelect,
+  CustomTextArea,
+  CustomTextField,
+  Navbar,
+};
