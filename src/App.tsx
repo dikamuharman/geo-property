c623e9872e3433ac8db29f5f6d19ccc74a330c05
dikamuharman <dika1254@gmail.com>
2023-11-27 10:04:29 +0700

    chore : Add @tanstack/react-query dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import '@fontsource/lato';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import router from './config/router/routes';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <ChakraProvider
-      theme={extendTheme({
-        fonts: {
-          heading: `'Lato', 'sans-serif'`,
-          body: `'Lato', 'sans-serif'`,
-        },
-      })}
-    >
-      <RouterProvider router={router} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider
+        theme={extendTheme({
+          fonts: {
+            heading: `'Lato', 'sans-serif'`,
+            body: `'Lato', 'sans-serif'`,
+          },
+        })}
+      >
+        <RouterProvider router={router} />
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
