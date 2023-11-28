008a4e1dac98c0ccae0661033a9688f2524d50da
dikamuharman <dika1254@gmail.com>
2023-11-28 13:55:51 +0700

    Add Link component to Logo

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -1,12 +1,13 @@
 import { Flex, Heading, Image } from '@chakra-ui/react';
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 import LogoImage from '../../assets/logo.png';
 
 interface LogoProps {}
 
 const Logo: FC<LogoProps> = () => {
   return (
-    <Flex>
+    <Flex as={Link} to="/">
       <Image src={LogoImage} alt="Logo" w="50px" />
       <Heading as="h1" size="lg">
         GeoProperty
