9ce95dbc801998aadd388ade628bd24a3a3df555
dikamuharman <dika1254@gmail.com>
2023-11-28 11:06:53 +0700

    refactor : DrawMap component and add latLng display

--- src/features/addProperty/component/molecules/DrawMap.tsx
+++ src/features/addProperty/component/molecules/DrawMap.tsx
@@ -1,16 +1,17 @@
-import { Box, VStack } from "@chakra-ui/react";
-import { FC, useEffect } from "react";
-import Map from "../../../map";
-import MapboxDraw from "@mapbox/mapbox-gl-draw";
-import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
-import useMapStore from "../../../map/store/useMapStore";
-import * as turf from "@turf/turf";
+import { Box, HStack, Input, VStack } from '@chakra-ui/react';
+import MapboxDraw from '@mapbox/mapbox-gl-draw';
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
+import * as turf from '@turf/turf';
+import { FC, useEffect, useState } from 'react';
+import Map from '../../../map';
+import useMapStore from '../../../map/store/useMapStore';
 
 interface DrawMapProps {}
 
 const DrawMap: FC<DrawMapProps> = () => {
   // cara expose map menggunakan zustand
   const map = useMapStore((state) => state.map);
+  const [latLng, setLatLng] = useState<[number, number]>([0, 0]);
 
   useEffect(() => {
     if (!map) return;
@@ -20,7 +21,7 @@ const DrawMap: FC<DrawMapProps> = () => {
         polygon: true,
         trash: true,
       },
-      defaultMode: "draw_polygon",
+      defaultMode: 'draw_polygon',
     });
 
     map.addControl(draw);
@@ -34,26 +35,32 @@ const DrawMap: FC<DrawMapProps> = () => {
         const rounded_area = Math.round(area * 100) / 100; // m2
         console.log(rounded_area);
         for (const feature of data.features) {
-          if (feature.geometry.type === "Polygon") {
+          if (feature.geometry.type === 'Polygon') {
             const geom = feature.geometry.coordinates;
-            console.log(geom);
+            const poly = turf.polygon(geom);
+            const center = turf.centroid(poly);
+            setLatLng(center.geometry.coordinates as [number, number]);
           }
         }
       }
     };
 
-    map.on("draw.create", calculateArea);
+    map.on('draw.create', calculateArea);
 
-    map.on("draw.update", calculateArea);
+    map.on('draw.update', calculateArea);
 
-    map.on("draw.delete", calculateArea);
+    map.on('draw.delete', calculateArea);
   }, [map]);
 
   return (
-    <VStack w="full" alignItems="flex-start">
+    <VStack w="full" alignItems="flex-start" gap={4}>
       <Box w="full" h={500} borderRadius={8} overflow="hidden">
         <Map />
       </Box>
+      <HStack w="full">
+        <Input disabled value={latLng[0]} size="lg" />
+        <Input disabled value={latLng[1]} size="lg" />
+      </HStack>
     </VStack>
   );
 };
