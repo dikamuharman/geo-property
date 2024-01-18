dcf9dacf198572ab176c9eeaf5397b98b57fd9f0
Asnur Ramdani <asnurramdhani12@gmail.com>
2024-01-18 12:19:47 +0700

    Refactor import statements and add React.StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import 'mapbox-gl/dist/mapbox-gl.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import "mapbox-gl/dist/mapbox-gl.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    //<React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <App />
-    //</React.StrictMode>
+  </React.StrictMode>
 );
