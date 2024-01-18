21d20fa279eed92541cbb90cc29e9fde63115dc6
abiyyu03 <abiyyu652@gmail.com>
2024-01-18 09:53:37 +0700

    remove unused commented code in panorama

--- src/features/detail/component/Panorama.tsx
+++ src/features/detail/component/Panorama.tsx
@@ -10,62 +10,6 @@ const Panorama = ({ source }: PropsPanoramaType) => {
     const panoramaRef = useRef<HTMLDivElement | null>(null);
     const [textureLoaded, setTextureLoaded] = useState(false);
 
-    //     const panoramaTexture = new THREE.TextureLoader().load(source);
-    //     const panoramaRadius = (window.innerWidth * 0.9) / 2;
-    //     const geometry = new THREE.SphereGeometry(panoramaRadius, 60, 40);
-    //     const material = new THREE.MeshBasicMaterial({ map: panoramaTexture, side: THREE.DoubleSide });
-    //     const sphere = new THREE.Mesh(geometry, material);
-
-    //     const scene = new THREE.Scene();
-    //     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    //     const renderer = new THREE.WebGLRenderer();
-    //     const initializePanorama = () => {
-    //         renderer.setSize(window.innerWidth, window.innerHeight);
-
-    //         scene.add(sphere);
-
-    //         camera.position.set(0, 0, 0.1);
-
-    //         panoramaRef.current.appendChild(renderer.domElement);
-
-    //         // Add OrbitControls
-    //         const controls = new OrbitControls(camera, renderer.domElement);
-    //         controls.enableZoom = false;
-    //     };
-
-    //     const addCenterIcon = () => {
-    //         const text = document.createElement('h2');
-    //         text.innerText = "Kontrol dengan Mouse anda"; // Replace with the path to your text image
-    //         text.style.position = 'absolute';
-    //         text.style.height = '50px';
-    //         text.style.top = '50%';
-    //         text.style.left = '50%';
-    //         text.style.zIndex = '20';
-
-    //         panoramaRef.current.appendChild(text);
-    //     };
-
-    //     const handleResize = () => {
-    //         const newWidth = window.innerWidth;
-    //         const newHeight = window.innerHeight;
-
-    //         camera.aspect = newWidth / newHeight;
-    //         camera.updateProjectionMatrix();
-
-    //         renderer.setSize(newWidth, newHeight);
-    //     };
-
-    //     // Initialize panorama only once on mount
-    //     if (!panoramaInitialized && textureLoaded) {
-    //         initializePanorama();
-    //         addCenterIcon();
-    //         setPanoramaInitialized(true);
-    //     }
-    //     setTextureLoaded(true);
-
-    //     // Resize event listener
-    //     window.addEventListener('resize', handleResize);
-    // }, [textureLoaded, panoramaInitialized]);
     useEffect(() => {
         const panoramaTexture = new THREE.TextureLoader().load(source);
         const geometry = new THREE.SphereGeometry((window.innerWidth * 0.5) / 2, 60, 40);
@@ -76,9 +20,7 @@ const Panorama = ({ source }: PropsPanoramaType) => {
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         const renderer = new THREE.WebGLRenderer();
         if (!textureLoaded) {
-            // renderer.setSize(window.innerWidth * 0.95, window.innerHeight * 0.8);
-            // renderer.domElement.className = 'w-44';
-
+            c
             scene.add(sphere);
 
             camera.position.set(0, 0, 0.1);
@@ -125,9 +67,6 @@ const Panorama = ({ source }: PropsPanoramaType) => {
             animate();
             return () => {
                 window.removeEventListener('resize', handleResize);
-
-                // You might need to dispose of other resources depending on your application
-
                 // Dispose of WebGLRenderer
                 if (renderer) {
                     // renderer.domElement.removeEventListener('dblclick', onDoubleClick);
@@ -137,7 +76,6 @@ const Panorama = ({ source }: PropsPanoramaType) => {
 
         }
     }, [source, textureLoaded]);
-    // useEffect(() => {
     //     let scene: undefined, camera: undefined, renderer: undefined, controls: undefined;
 
     //     const initializePanorama = () => {
@@ -194,7 +132,7 @@ const Panorama = ({ source }: PropsPanoramaType) => {
     //     };
     // }, [textureLoaded]);
     return (
-        <div className='w-44'>
+        <div>
             <div ref={panoramaRef} />
         </div>
     );
